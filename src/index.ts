4ae1613a0ec916f375009f355545cd0417c1ffe8
sky <skybride@hotmail.com>
2021-08-29 18:53:17 -0400

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+    res.send('Works!');
+});
+
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}`);
+});
